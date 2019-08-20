1ff8d7894f76def153a0bb1dda8417eee14a9e4a
tirthjain <jaintirth24@gmail.com>
2019-08-20 14:25:15 +0530

    host.sh now hosts directly from given project directory.

--- django-autohost/host.sh
+++ django-autohost/host.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-export PROJECT_NAME=$1
-export PROJECT_DIR=$2
-export HOST_IP=$3
-export HOST_PORT=$4
-
-sudo mkdir /srv/$PROJECT_NAME
-sudo mkdir /srv/$PROJECT_NAME/run
-sudo mkdir /srv/$PROJECT_NAME/logs
-sudo touch /srv/$PROJECT_NAME/logs/gunicorn-supervisor.log
-sudo touch /srv/$PROJECT_NAME/logs/nginx-access.log
-sudo touch /srv/$PROJECT_NAME/logs/nginx-error.log
-
-sudo cp -r $PROJECT_DIR /srv/$PROJECT_NAME
-
-#-------------------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
+# Get the following variables from arguments in the command line.
+export PROJECT_NAME=$1 # Name of project.
+export PROJECT_DIR=$2 # Path to project.
+export HOST_IP=$3 # Hosting ip
+export HOST_PORT=$4 # Port
 
 echo "Setting up gunicorn..."
+GUNICORN_SCRIPT="${PROJECT_DIR}/gunicorn-start.sh"
 
-touch /srv/${PROJECT_NAME}/gunicorn-start.sh
+rm -rf $GUNICORN_SCRIPT
+touch $GUNICORN_SCRIPT
 
-cat >> /srv/${PROJECT_NAME}/gunicorn-start.sh <<\EOF
+cat >> $GUNICORN_SCRIPT <<\EOF
 #!/bin/bash
 EOF
 
-cat >> /srv/${PROJECT_NAME}/gunicorn-start.sh <<EOF
-NAME="${PROJECT_NAME}"
-DJANGODIR=/srv/${PROJECT_NAME}/${PROJECT_NAME}
+#-------------------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
+
+cat >> $GUNICORN_SCRIPT <<EOF
+NAME=${PROJECT_NAME}
+DJANGODIR=${PROJECT_DIR}
 SOCKFILE=/srv/${PROJECT_NAME}/run/gunicorn.sock
 USER=root
 GROUP=webdata
@@ -35,9 +29,9 @@ DJANGO_SETTINGS_MODULE=${PROJECT_NAME}.settings
 DJANGO_WSGI_MODULE=${PROJECT_NAME}.wsgi
 EOF
 
-cat >> /srv/${PROJECT_NAME}/gunicorn-start.sh <<\EOF
+cat >> $GUNICORN_SCRIPT <<\EOF
 echo "Starting $NAME as `whoami`"
-source /srv/${NAME}/venv/bin/activate
+source $DJANGODIR/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
@@ -45,48 +39,42 @@ RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $NAME \
+  --name $NAME\
   --workers $NUM_WORKERS \
   --user $USER \
+  --group www-data
   --bind=unix:$SOCKFILE
 
 EOF
 
-sudo chmod u+x /srv/$PROJECT_NAME/gunicorn-start.sh
+sudo chmod u+x $GUNICORN_SCRIPT
 
 
-
-#---------------------------------------------------------------SETUP PROJECT ENVIRONMENT--------------------------------------------------------------
-
-# cd /srv/$PROJECT_NAME
-python3 -m venv /srv/$PROJECT_NAME/venv
-source /srv/$PROJECT_NAME/venv/bin/activate
-pip install -r /srv/$PROJECT_NAME/$PROJECT_NAME/requirements.txt
-pip install gunicorn
-
-python $PROJECT_NAME/manage.py collectstatic
+python3 -m venv $PROJECT_DIR/venv
+source $PROJECT_DIR/venv/bin/activate
+pip install -r $PROJECT_DIR/requirements.txt
+python $PROJECT_DIR/manage.py collectstatic
 
 deactivate
 
 
-#--------------------------------------------------------------SETUP SERVER-------------------------------------------------------------------------
+#---------------------------------------------------------------SETUP PROJECT ENVIRONMENT--------------------------------------------------------------
 
-echo "installing dependencies..."
-sudo apt-get install nginx
-sudo apt-get install supervisor
+echo "Installing dependencies..."
+sudo apt-get install nginx supervisor
+
 echo "  "
-
 echo "Setting up supervisor"
 echo $PROJECT_NAME.conf
 
-sudo rm -rf /etc/supervisor/conf.d/$PROJECT_NAME.conf
-touch /etc/supervisor/conf.d/$PROJECT_NAME.conf
-
-sudo cat >> /etc/supervisor/conf.d/$PROJECT_NAME.conf <<EOF
+SUPERVISOR_CONF=/etc/supervisor/conf.d/${PROJECT_NAME}.conf
+sudo rm -rf $SUPERVISOR_CONF
+sudo touch $SUPERVISOR_CONF
+sudo cat >> $SUPERVISOR_CONF <<EOF
 [program:$PROJECT_NAME]
-command = /srv/$PROJECT_NAME/gunicorn-start.sh
+command = $GUNICORN_SCRIPT
 user = root
-stdout_logfile = /srv/${PROJECT_NAME}/logs/gunicorn-supervisor.log
+stdout_file = $PROJECT_DIR/gunicorn-supervisor.log
 redirect_stderr = true
 EOF
 
@@ -96,37 +84,40 @@ sudo supervisorctl update
 echo "  "
 echo "Setting up nginx"
 
-sudo rm -rf /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
-touch /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
+sudo mkdir -p ${PROJECT_DIR}/logs
+NGINX_CONF=/etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
 
-sudo cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<EOF
+sudo rm -rf $NGINX_CONF
+sudo touch $NGINX_CONF
+
+sudo cat >> $NGINX_CONF <<EOF
 
 upstream ${PROJECT_NAME}_app_server {
-  server unix:/srv/${PROJECT_NAME}/run/gunicorn.sock fail_timeout=0;
+  server unix:$PROJECT_DIR/run/${PROJECT_NAME}.sock fail_timeout=0;
 }
 
 server {
 
-	listen $HOST_PORT;
-	server_name $HOST_IP;
-	
-	client_max_body_size 4G;
-	
-	access_log /srv/${PROJECT_NAME}/logs/nginx-access.log;
-	error_log /srv/${PROJECT_NAME}/logs/nginx-error.log;
-	
-	location /static/ {
-		alias	/srv/$PROJECT_NAME/$PROJECT_NAME/static/;
-	}
-	
-	location /media/ {
-		alias	/srv/$PROJECT_NAME/$PROJECT_NAME/media;
-	}
+  listen $HOST_PORT;
+  server_name $HOST_IP;
+
+  client_max_body_size 4G;
+
+  access_log $PROJECT_DIR/logs/nginx-access.log;
+  error_log $PROJECT_DIR/logs/nginx-error.log;
+
+  location /static/ {
+    alias $PROJECT_DIR/static/;
+  }
+
+  location /media/ {
+    alias $PROJECT_DIR/media/;
+}
 EOF
 
 cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<\EOF
 
-	location / {
+  location / {
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header Host $http_host;
 
@@ -157,5 +148,5 @@ sudo service nginx restart
 sudo service supervisor restart
 
 echo "  "
-echo "Your django project is online on ${HOST_IP}:${HOST_PORT}"
+echo "Your django project is online on ${HOST_IP}:${HOST_PORT}" 
 
