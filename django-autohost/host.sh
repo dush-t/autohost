070e7636eb98a058cf6a494d6d2411d70858cc78
tirthjain <jaintirth24@gmail.com>
2019-08-20 22:32:26 +0530

    Fixes sockfile bug
    -Fixes the bug because of which sock file wasn’t getting created
    leading to a 404 error.
    -Stores all the hosting files in a separate directory
    (/home/autohost/<project_name>_hostingdata)

--- django-autohost/host.sh
+++ django-autohost/host.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# Get the following variables from arguments in the command line.
-export PROJECT_NAME=$1 # Name of project.
-export PROJECT_DIR=$2 # Path to project.
-export HOST_IP=$3 # Hosting ip
-export HOST_PORT=$4 # Port
+
+# Command line arguments.
+export PROJECT_NAME=$1
+export PROJECT_DIR=$2
+export HOST_IP=$3
+export HOST_PORT=$4
 
 
-#-------------------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
+HOSTING_DATA_DIR="/home/autohost/${PROJECT_NAME}_hostingdata"
+sudo mkdir -p $HOSTING_DATA_DIR
 
-echo "Setting up gunicorn..."
-GUNICORN_SCRIPT="${PROJECT_DIR}/gunicorn-start.sh"
+sudo chown -p `whoami` $HOSTING_DATA_DIR
+
+python3 -m venv
+
+#-------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
+
+echo "Setting up gunicorn"
+GUNICORN_SCRIPT="${HOSTING_DATA_DIR}/gunicorn-start.sh"
+
+sudo apt-get install python-virtualenv
+python3 -m venv $HOSTING_DATA_DIR/venv
+source $HOSTING_DATA_DIR/venv/bin/activate
+pip install -r $PROJECT_DIR/requirements.txt
+pip install -r gunicorn.sh
+python $PROJECT_DIR/manage.py collectstatic
 
 rm -rf $GUNICORN_SCRIPT
 touch $GUNICORN_SCRIPT
@@ -20,9 +35,13 @@ cat >> $GUNICORN_SCRIPT <<\EOF
 EOF
 
 cat >> $GUNICORN_SCRIPT <<EOF
+source ${HOSTING_DATA_DIR}/venv/bin/activate
+EOF
+
+cat >> $GUNICORN_SCRIPT <<EOF
 NAME=${PROJECT_NAME}
 DJANGODIR=${PROJECT_DIR}
-SOCKFILE=/srv/${PROJECT_NAME}/run/gunicorn.sock
+SOCKFILE=$HOSTING_DATA_DIR/run/gunicorn.sock
 USER=root
 GROUP=webdata
 NUM_WORKERS=4
@@ -32,7 +51,7 @@ EOF
 
 cat >> $GUNICORN_SCRIPT <<\EOF
 echo "Starting $NAME as `whoami`"
-source $DJANGODIR/venv/bin/activate
+source $HOSTING_DATA_DIR/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
@@ -40,42 +59,32 @@ RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $NAME\
+  --name $NAME \
   --workers $NUM_WORKERS \
   --user $USER \
-  --group www-data
-  --bind=unix:$SOCKFILE
-
+  --bind unix:$SOCKFILE
 EOF
 
 sudo chmod u+x $GUNICORN_SCRIPT
 
-
-python3 -m venv $PROJECT_DIR/venv
-source $PROJECT_DIR/venv/bin/activate
-pip install -r $PROJECT_DIR/requirements.txt
-python $PROJECT_DIR/manage.py collectstatic
-
 deactivate
 
-
-#---------------------------------------------------------------SETUP PROJECT ENVIRONMENT--------------------------------------------------------------
-
-echo "Installing dependencies..."
+#-------------------------------------------------------SETUP PROJECT ENVIRONMENT---------------------------------------------------------
+echo "Installing dependencies"
 sudo apt-get install nginx supervisor
 
 echo "  "
 echo "Setting up supervisor"
-echo $PROJECT_NAME.conf
 
 SUPERVISOR_CONF=/etc/supervisor/conf.d/${PROJECT_NAME}.conf
 sudo rm -rf $SUPERVISOR_CONF
 sudo touch $SUPERVISOR_CONF
-sudo cat >> $SUPERVISOR_CONF <<EOF
-[program:$PROJECT_NAME]
+
+sudo cat >> SUPERVISOR_CONF <<EOF
+[program:${PROJECT_NAME}]
 command = $GUNICORN_SCRIPT
 user = root
-stdout_file = $PROJECT_DIR/gunicorn-supervisor.log
+stdout_file = ${HOSTING_DATA_DIR}/gunicorn-supervisor.log
 redirect_stderr = true
 EOF
 
@@ -85,7 +94,8 @@ sudo supervisorctl update
 echo "  "
 echo "Setting up nginx"
 
-sudo mkdir -p ${PROJECT_DIR}/logs
+sudo mkdir -p ${HOSTING_DATA_DIR}/logs
+
 NGINX_CONF=/etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
 
 sudo rm -rf $NGINX_CONF
@@ -94,7 +104,7 @@ sudo touch $NGINX_CONF
 sudo cat >> $NGINX_CONF <<EOF
 
 upstream ${PROJECT_NAME}_app_server {
-  server unix:$PROJECT_DIR/run/${PROJECT_NAME}.sock fail_timeout=0;
+  server unix:${HOSTING_DATA_DIR}/run/${PROJECT_NAME}.sock fail_timeout=0;
 }
 
 server {
@@ -104,8 +114,8 @@ server {
 
   client_max_body_size 4G;
 
-  access_log $PROJECT_DIR/logs/nginx-access.log;
-  error_log $PROJECT_DIR/logs/nginx-error.log;
+  access_log $HOSTING_DATA_DIR/logs/nginx-access.log;
+  error_log $HOSTING_DATA_DIR/logs/nginx-error.log
 
   location /static/ {
     alias $PROJECT_DIR/static/;
@@ -113,41 +123,38 @@ server {
 
   location /media/ {
     alias $PROJECT_DIR/media/;
-}
+  }
 EOF
 
-cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<\EOF
-
+cat >> $NGINX_CONF <<\EOF
   location / {
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header Host $http_host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $http_host;
 
-          proxy_redirect off;
-        if (!-f $request_filename) {
+    proxy_redirect off;
+    if (!-f $request_filename) {
 EOF
 
-cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<EOF
-
-            proxy_pass http://${PROJECT_NAME}_app_server;
-            break;
-        }
+cat >> $NGINX_CONF <<EOF
+      proxy_pass https://${PROJECT_NAME}_app_server;
+      break;
     }
+  }
 
-    # Error pages
-    error_page 500 502 503 504 /500.html;
-    location = /500.html {
-        root ${SCRIPT_PATH}/static/;
-    }
+  # Error pages
+  error_page 500 502 503 504 /500.html;
+  location = /500.html {
+    root ${PROJECT_DIR}/static/;
+  }
 }
 
 EOF
 
 echo "  "
-echo "Finishing up..."
 
 sudo service nginx restart
 sudo service supervisor restart
 
 echo "  "
-echo "Your django project is online on ${HOST_IP}:${HOST_PORT}" 
+echo "Your django project is online on ${HOST_IP}:${HOST_PORT}"
 
