5d127e132e06170b9c2e88ea6fcf7676dd56c251
tirthjain <jaintirth24@gmail.com>
2019-08-21 20:32:55 +0530

    .

--- django-autohost/host.sh
+++ django-autohost/host.sh
@@ -5,88 +5,95 @@ export PROJECT_DIR=$2
 export HOST_IP=$3
 export HOST_PORT=$4
 
-sudo mkdir /srv/$PROJECT_NAME
-sudo mkdir /srv/$PROJECT_NAME/run
-sudo mkdir /srv/$PROJECT_NAME/logs
-sudo touch /srv/$PROJECT_NAME/logs/gunicorn-supervisor.log
-sudo touch /srv/$PROJECT_NAME/logs/nginx-access.log
-sudo touch /srv/$PROJECT_NAME/logs/nginx-error.log
+HOSTING_DIR="/home/autohost/${PROJECT_NAME}_hostingdata"
 
-sudo cp -r $PROJECT_DIR /srv/$PROJECT_NAME
+echo "Saving hosting files at: ${HOSTING_DIR}"
 
-#-------------------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
+sudo mkdir -p "$HOSTING_DIR"
+
+sudo chgrp www-data  "$HOSTING_DIR"
+#-------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
 
 echo "Setting up gunicorn..."
 
-touch /srv/${PROJECT_NAME}/gunicorn-start.sh
+sudo rm -rf ${HOSTING_DIR}/gunicorn-start.sh
+sudo touch ${HOSTING_DIR}/gunicorn-start.sh
 
-cat >> /srv/${PROJECT_NAME}/gunicorn-start.sh <<\EOF
-#!/bin/bash
+cat >> ${HOSTING_DIR}/gunicorn-start.sh << \EOF
+#!bin/bash
 EOF
 
-cat >> /srv/${PROJECT_NAME}/gunicorn-start.sh <<EOF
-NAME="${PROJECT_NAME}"
-DJANGODIR=/srv/${PROJECT_NAME}/${PROJECT_NAME}
-SOCKFILE=/srv/${PROJECT_NAME}/run/gunicorn.sock
+cat >> ${HOSTING_DIR}/gunicorn-start.sh <<EOF
+NAME=${PROJECT_NAME}
+DJANGODIR=${PROJECT_DIR}
+SOCKFILE=${HOSTING_DIR}/run/gunicorn.sock
 USER=root
-GROUP=webdata
+GROUP=www-data
 NUM_WORKERS=4
 DJANGO_SETTINGS_MODULE=${PROJECT_NAME}.settings
 DJANGO_WSGI_MODULE=${PROJECT_NAME}.wsgi
 EOF
 
-cat >> /srv/${PROJECT_NAME}/gunicorn-start.sh <<\EOF
+cat >> ${HOSTING_DIR}/gunicorn-start.sh <<\EOF
 echo "Starting $NAME as `whoami`"
+EOF
+
+cat >> ${HOSTING_DIR}/gunicorn-start.sh <<EOF
+source ${HOSTING_DIR}/venv/bin/activate
+EOF
+
+cat  >> ${HOSTING_DIR}/gunicorn-start.sh <<\EOF
+echo "Starting $NAME as $(whoami)"
 source /srv/${NAME}/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+EOF
 
-RUNDIR=$(dirname $SOCKFILE)
+cat >> ${HOSTING_DIR}/gunicorn-start.sh <<EOF
+RUNDIR=${HOSTING_DIR}/run
+EOF
+
+cat >> ${HOSTING_DIR}/gunicorn-start.sh <<\EOF
 test -d $RUNDIR || mkdir -p $RUNDIR
-
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user $USER \
+  --group $GROUP
   --bind=unix:$SOCKFILE
-
 EOF
 
-sudo chmod u+x /srv/$PROJECT_NAME/gunicorn-start.sh
+sudo chmod u+x ${HOSTING_DIR}/gunicorn-start.sh
 
+#-------------------------------------------------------SETUP PROJECT ENVIRONMENT---------------------------------------------------------
+echo "Setting up virtual environment..."
+python3 -m venv "$HOSTING_DIR"/venv
+source "$HOSTING_DIR"/venv/bin/activate
+pip3 install -r "${PROJECT_DIR}"/requirements.txt
+pip3 install gunicorn
 
-
-#---------------------------------------------------------------SETUP PROJECT ENVIRONMENT--------------------------------------------------------------
-
-# cd /srv/$PROJECT_NAME
-python3 -m venv /srv/$PROJECT_NAME/venv
-source /srv/$PROJECT_NAME/venv/bin/activate
-pip install -r /srv/$PROJECT_NAME/$PROJECT_NAME/requirements.txt
-pip install gunicorn
-
-python $PROJECT_NAME/manage.py collectstatic
+python3 "${PROJECT_DIR}"/manage.py collectstatic
 
 deactivate
 
-
-#--------------------------------------------------------------SETUP SERVER-------------------------------------------------------------------------
-
-echo "installing dependencies..."
+#--------------------------------------------------------------SETUP SUPERVISOR----------------------------------------------------------------
+echo "Installing dependencies..."
 sudo apt-get install nginx
 sudo apt-get install supervisor
+
 echo "  "
 
-echo "Setting up supervisor"
-echo $PROJECT_NAME.conf
+echo "Setting up supervisor..."
+echo "$PROJECT_NAME".conf
 
-sudo rm -rf /etc/supervisor/conf.d/$PROJECT_NAME.conf
-touch /etc/supervisor/conf.d/$PROJECT_NAME.conf
+sudo rm -rf /etc/supervisor/conf.d/${PROJECT_NAME}.conf
+sudo touch /etc/supervisor/conf.d/${PROJECT_NAME}.conf
 
-sudo cat >> /etc/supervisor/conf.d/$PROJECT_NAME.conf <<EOF
+sudo cat >> /etc/supervisor/conf.d/${PROJECT_NAME}.conf <<EOF
 [program:$PROJECT_NAME]
-command = /srv/$PROJECT_NAME/gunicorn-start.sh
+command = $GUNICORN_SCRIPT
 user = root
-stdout_logfile = /srv/${PROJECT_NAME}/logs/gunicorn-supervisor.log
+stdout_file = ${HOSTING_DATA_DIR}/logs/gunicorn-supervisor.log
 redirect_stderr = true
 EOF
 
@@ -94,61 +101,62 @@ sudo supervisorctl reread
 sudo supervisorctl update
 
 echo "  "
+
+#--------------------------------------------------------------SETUP NGINX----------------------------------------------------------------
+
 echo "Setting up nginx"
 
+sudo mkdir -p ${HOSTING_DATA_DIR}/logs
+
 sudo rm -rf /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
-touch /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
+sudo touch /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
 
-sudo cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<EOF
-
+sudo cat >> /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf <<EOF
 upstream ${PROJECT_NAME}_app_server {
-  server unix:/srv/${PROJECT_NAME}/run/gunicorn.sock fail_timeout=0;
+server unix:${HOSTING_DATA_DIR}/run/${PROJECT_NAME}.sock fail_timeout=0;
 }
-
 server {
-
-	listen $HOST_PORT;
-	server_name $HOST_IP;
-	
-	client_max_body_size 4G;
-	
-	access_log /srv/${PROJECT_NAME}/logs/nginx-access.log;
-	error_log /srv/${PROJECT_NAME}/logs/nginx-error.log;
-	
-	location /static/ {
-		alias	/srv/$PROJECT_NAME/$PROJECT_NAME/static/;
-	}
-	
-	location /media/ {
-		alias	/srv/$PROJECT_NAME/$PROJECT_NAME/media;
-	}
-EOF
-
-cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<\EOF
-
-	location / {
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header Host $http_host;
-
-          proxy_redirect off;
-        if (!-f $request_filename) {
-EOF
-
-cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<EOF
-
-            proxy_pass http://${PROJECT_NAME}_app_server;
-            break;
-        }
-    }
-
-    # Error pages
-    error_page 500 502 503 504 /500.html;
-    location = /500.html {
-        root ${SCRIPT_PATH}/static/;
-    }
+listen $HOST_PORT;
+server_name $HOST_IP;
+client_max_body_size 4G;
+access_log $HOSTING_DATA_DIR/logs/nginx-access.log;
+error_log $HOSTING_DATA_DIR/logs/nginx-error.log;
+location /static/ {
+alias $PROJECT_DIR/static/;
 }
-
+location /media/ {
+alias $PROJECT_DIR/media/;
+}
 EOF
+
+cat >> /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf <<\EOF
+location / {
+proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+proxy_set_header Host $http_host;
+proxy_redirect off;
+if (!-f $request_filename) {
+EOF
+
+cat >> /etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf <<EOF
+proxy_pass http://${PROJECT_NAME}_app_server;
+break;
+}
+}
+# Error pages
+error_page 500 502 503 504 /500.html;
+location = /500.html {
+root ${PROJECT_DIR}/static/;
+}
+}
+EOF
+
+echo "  "
+
+sudo service nginx restart
+sudo service supervisor restart
+
+echo "  "
+echo "Your django project is online on ${HOST_IP}:${HOST_PORT}"
 
 echo "  "
 echo "Finishing up..."
@@ -158,4 +166,3 @@ sudo service supervisor restart
 
 echo "  "
 echo "Your django project is online on ${HOST_IP}:${HOST_PORT}"
-
