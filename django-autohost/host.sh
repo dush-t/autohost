d6bbd5a0cb69344a09f766e5517f871047728afd
tirthjain <jaintirth24@gmail.com>
2019-08-21 19:40:28 +0530

    Fixed.

--- django-autohost/host.sh
+++ django-autohost/host.sh
@@ -9,21 +9,21 @@ HOSTING_DIR=/home/autohost/${PROJECT_NAME}_hostingdata
 
 echo "Saving hosting files at: ${HOSTING_DIR}"
 
-sudo mkdir -p $HOSTING_DIR
+sudo mkdir -p "$HOSTING_DIR"
 
 #-------------------------------------------------------SETUP GUNICORN--------------------------------------------------------------------
 
 echo "Setting up gunicorn..."
 GUNICORN_SCRIPT=${HOSTING_DIR}/gunicorn-start.sh
 
-sudo rm -rf $GUNICORN_SCRIPT
-sudo touch $GUNICORN_SCRIPT
+sudo rm -rf "$GUNICORN_SCRIPT"
+sudo touch "$GUNICORN_SCRIPT"
 
-cat >> $GUNICORN_SCRIPT << \EOF
+cat >> "$GUNICORN_SCRIPT" << \EOF
 #!bin/bash
 EOF
 
-cat >> $GUNICORN_SCRIPT <<EOF
+cat >> "$GUNICORN_SCRIPT" <<EOF
 NAME=${PROJECT_NAME}
 DJANGODIR=${PROJECT_DIR}
 SOCKFILE=${HOSTING_DIR}/run/gunicorn.sock
@@ -34,25 +34,25 @@ DJANGO_SETTINGS_MODULE=${PROJECT_NAME}.settings
 DJANGO_WSGI_MODULE=${PROJECT_NAME}.wsgi
 EOF
 
-cat >> $GUNICORN_SCRIPT <<\EOF
+cat >> "$GUNICORN_SCRIPT" <<\EOF
 echo 'Starting $NAME as `whoami`'
 EOF
 
-cat >> $GUNICORN_SCRIPT <<EOF
+cat >> "$GUNICORN_SCRIPT" <<EOF
 source ${HOSTING_DIR}/venv/bin/activate
 
 cat  >> $GUNICORN_SCRIPT <<\EOF
-echo "Starting $NAME as `whoami`"
+echo "Starting $NAME as $(whoami)"
 source /srv/${NAME}/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 EOF
 
-cat >> $GUNICORN_SCRIPT <<EOF
+cat >> "$GUNICORN_SCRIPT" <<EOF
 RUNDIR=${HOSTING_DIR}/run
 EOF
 
-cat >> $GUNICORN_SCRIPT <<\EOF
+cat >> "$GUNICORN_SCRIPT" <<\EOF
 test -d $RUNDIR || mkdir -p $RUNDIR
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
@@ -61,16 +61,16 @@ exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --bind=unix:$SOCKFILE
 EOF
 
-sudo chmod u+x $GUNICORN_SCRIPT
+sudo chmod u+x "$GUNICORN_SCRIPT"
 
 #-------------------------------------------------------SETUP PROJECT ENVIRONMENT---------------------------------------------------------
 echo "Setting up virtual environment..."
-python3 -m venv $HOSTING_DIR/venv
-source $HOSTING_DIR/venv/bin/activate
-pip3 install -r ${PROJECT_DIR}/requirements.txt
+python3 -m venv "$HOSTING_DIR"/venv
+source "$HOSTING_DIR"/venv/bin/activate
+pip3 install -r "${PROJECT_DIR}"/requirements.txt
 pip3 install gunicorn
 
-python3 ${PROJECT_DIR}/manage.py collectstatic
+python3 "${PROJECT_DIR}"/manage.py collectstatic
 
 deactivate
 
@@ -82,14 +82,14 @@ sudo apt-get install supervisor
 echo "  "
 
 echo "Setting up supervisor..."
-echo $PROJECT_NAME.conf
+echo "$PROJECT_NAME".conf
 
-$SUPERVISOR_CONF=/etc/supervisor/conf.d/${PROJECT_NAME}.conf
+SUPERVISOR_CONF="/etc/supervisor/conf.d/${PROJECT_NAME}.conf"
 
-sudo rm -rf $SUPERVISOR_CONF
-sudo touch $SUPERVISOR_CONF
+sudo rm -rf "$SUPERVISOR_CONF"
+sudo touch "$SUPERVISOR_CONF"
 
-sudo cat >> $SUPERVISOR_CONF <<EOF
+sudo cat >> "$SUPERVISOR_CONF" <<EOF
 [program:$PROJECT_NAME]
 command = $GUNICORN_SCRIPT
 user = root
@@ -107,10 +107,10 @@ echo "  "
 echo "Setting up nginx"
 NGINX_CONF=/etc/nginx/sites-enabled/${PROJECT_NAME}.nginxconf
 
-sudo rm -rf $NGINX_CONF
-sudo touch $NGINX_CONF
+sudo rm -rf "$NGINX_CONF"
+sudo touch "$NGINX_CONF"
 
-sudo cat >> $NGINX_CONF <<EOF
+sudo cat >> "$NGINX_CONF" <<EOF
 
 upstream ${PROJECT_NAME}_app_server {
   server unix:${HOSTING_DIR}/run/gunicorn.sock fail_timeout=0;
@@ -134,7 +134,7 @@ server {
   }
 EOF
 
-cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<\EOF
+cat >> /etc/nginx/sites-enabled/"$PROJECT_NAME".nginxconf <<\EOF
 	location / {
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header Host $http_host;
@@ -142,7 +142,7 @@ cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<\EOF
         if (!-f $request_filename) {
 EOF
 
-cat >> /etc/nginx/sites-enabled/$PROJECT_NAME.nginxconf <<EOF
+cat >> /etc/nginx/sites-enabled/"$PROJECT_NAME".nginxconf <<EOF
             proxy_pass http://${PROJECT_NAME}_app_server;
             break;
         }
